78d9c62610cbb51f98451dbdc9f7feb8e277dc07
juannhm02 <91215491+juannhm02@users.noreply.github.com>
2024-04-21 13:00:55 +0200

    Actualización pages

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // JavaScript opcional para agregar interactividad
+document.addEventListener('DOMContentLoaded', function() {
+    const changeThemeButton = document.getElementById('change-theme-btn');
+
+    if (changeThemeButton) {
+        changeThemeButton.addEventListener('click', function() {
+            document.body.classList.toggle('dark-theme');
+        });
+    }
+});
+
+// Función para alternar los estilos del tema oscuro
+function toggleDarkTheme() {
+    var body = document.body;
+    body.style.backgroundColor = body.style.backgroundColor === 'rgb(52, 58, 64)' ? '#f4f4f4' : 'rgb(52, 58, 64)';
+    body.style.color = body.style.color === 'rgb(255, 255, 255)' ? '#333' : 'rgb(255, 255, 255)';
+}
