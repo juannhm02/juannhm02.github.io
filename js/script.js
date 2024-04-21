e7acdfaeba86a569ce219e02743122ca3caec934
juannhm02 <91215491+juannhm02@users.noreply.github.com>
2024-04-21 14:32:03 +0200

    Cambios

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const changeThemeButton = document.getElementById('change-theme-btn');
-
-    if (changeThemeButton) {
-        changeThemeButton.addEventListener('click', function() {
-            document.body.classList.toggle('dark-theme');
-            var links = document.querySelectorAll('a');
-            links.forEach(link => {
-                link.classList.toggle('dark-theme');
-            });
-        });
-    }
-});
 {
-
+    document.addEventListener('DOMContentLoaded', function() {
+        const changeThemeButton = document.getElementById('change-theme-btn');
+    
+        if (changeThemeButton) {
+            changeThemeButton.addEventListener('click', function() {
+                document.body.classList.toggle('dark-theme');
+                var links = document.querySelectorAll('a');
+                links.forEach(link => {
+                    link.classList.toggle('dark-theme');
+                });
+            });
+        }
+    });
+    
     const fwSkills={
         MongoDB:['#01DF01',"70%"],
         React: ['#81DAF5',"30%"],
